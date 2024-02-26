cf042a7452a8bf51af8903bd9a87673e28a1e5c0
Emirfahimi0 <emirfahimi0@gmail.com>
2024-02-26 12:03:51 +0800

    feat: routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
 import "./index.css";
 import { reportWebVitals } from "./reportWebVitals"; // Import the function
 import { MovieProvider as GlobalProvider } from "./context";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { HomePage, MetricsPage, MovieDetailsPage } from "./layout/index.ts";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/MovieReview/",
+    element: <HomePage />,
+    children: [
+      {
+        path: "/MovieReview/Details",
+        element: <MovieDetailsPage />,
+      },
+      {
+        path: "/MovieReview/Metrics",
+        element: <MetricsPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalProvider>
-      <App />
+      <RouterProvider router={appRouter} />
     </GlobalProvider>
   </React.StrictMode>
 );
