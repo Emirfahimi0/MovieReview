6a9b4eb22448c6b937742ba9c1def7290982c0ec
Emirfahimi0 <emirfahimi0@gmail.com>
2024-02-26 18:50:38 +0800

    added types

--- src/types/movie.d.ts
+++ src/types/movie.d.ts
@@ -61,6 +61,24 @@ interface SpokenLanguage {
   name: string;
 }
 
+export interface IMovieRecommendation {
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  title: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  genre_ids: number[];
+  popularity: number;
+  release_date: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
 export type genreTypes = { id: number; name: string };
 
 export type ApiType = "Popular Movies API" | "Details Movie API";
