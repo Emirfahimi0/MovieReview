f62e66aa3fe54db2032a1c78c56f0880de8bb0c7
Emirfahimi0 <emirfahimi0@gmail.com>
2024-02-28 21:13:55 +0800

    added header section

---
+++ src/components/NavBar/Header.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent, useState } from "react";
+import Logo from "../../assets/img/Logo.png";
+import { NavItems } from "./NavItems";
+
+export const Header: FunctionComponent = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleToggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <header className="bg-slate-900 sticky top-0 z-[20] flex-wrap mx-auto flex w-full items-center justify-between">
+      <div className="logo h-16 w-16 my-2">
+        <img src={Logo} className="rounded-full" />
+      </div>
+      <NavItems isOpen={isOpen} handleToggle={handleToggleMenu} />
+    </header>
+  );
+};
