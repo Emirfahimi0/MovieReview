bb28b32df02211f94c3fec3b3cf4f6bd61056b4a
Emirfahimi0 <emirfahimi0@gmail.com>
2024-02-28 21:24:48 +0800

    remove unused package

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { MovieProvider as GlobalProvider } from "./context";
 import { App } from "./App.tsx";
-import ReactGA from "react-ga4";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -12,21 +11,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </GlobalProvider>
   </React.StrictMode>
 );
-
-ReactGA.initialize([
-  {
-    trackingId: "G-GQMSLTD3XR",
-  },
-]);
-
-ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Main" });
-
-// Send a custom event
-ReactGA.event({
-  category: "your category",
-  action: "your action",
-  label: "your label", // optional
-  value: 99, // optional, must be a number
-  nonInteraction: true, // optional, true/false
-  transport: "xhr", // optional, beacon/xhr/image
-});
